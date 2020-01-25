64b2b4ed96ca5edbd0fa834603a6bfdfb20f7bb4
Fattahul Akbar <akbarfattahul22@gmail.com>
2020-01-25 18:43:33 +0700

    Remove Connection Setting

--- config/connection.php
+++ config/connection.php
@@ -1,8 +1,8 @@
 <?php
 
-$server = "localhost";
-$username = "root";
-$password = "root";
-$database = "db_apotik_surya";
+$server = "isi dengan nama server anda";
+$username = "isi dengan username server anda";
+$password = "isi dengan password server anda";
+$database = "isi dengan nama database anda";
 
 $connection = mysqli_connect($server, $username, $password, $database);
